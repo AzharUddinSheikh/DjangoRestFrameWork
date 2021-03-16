1ca1d15efa74e7ab9d4b5739b6176fb6facbc9d6
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2021-03-17 00:58:28 +0530

    created update and delete functionality also provided path for that

--- Api/urls.py
+++ Api/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('task-list/', views.taskList, name='task-list'),
     path('task-detail/<int:pk>/', views.taskDetail, name='task-detail'),
     path('task-create/', views.taskCreate, name='task-create'),
+    path('task-update/<int:pk>/', views.taskUpdate, name='task-update'),
+    path('task-delete/<int:pk>/', views.taskDelete, name='task-delete'),
 ]
