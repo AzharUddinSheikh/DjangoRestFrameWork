275245f4ccd62583840ed3598ebcc18dbcd2cd4d
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2021-03-16 23:10:48 +0530

    model,py > created model, admin.py>registered created model, urls.py> provided path for given function, view.py>created function with return response

---
+++ Api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.apioverview, name='api-overview')
+]
--- Api/views.py
+++ Api/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+# def apioverview(request):
+#     """result ll be in json format"""
+#     return JsonResponse("API BASE POINT", safe=False)
+@api_view(['GET'])
+def apioverview(request):
+    api_urls = {
+        'List': '/task-list/',
+        'Detail View': '/task-detail/<str:pk>/',
+        'Create': '/task-create/',
+        'Update': '/task-update/<str:pk>/',
+        'Delete': '/task-delete/<str:pk>/',
+    }
+    return Response(api_urls)
