a11d276c351176e96db31b89804546c1d2e6a2fa
Azhar Uddin Sheikh <Azharthegreat123@gmail.com>
2021-03-16 23:58:30 +0530

    Working with Data-Serialization which is the process of converting structured data to a foramt that allows sharing or storage of the data in a form that allows recovery of its original structure, so created a file here TaskSerializer class is replicating a lot of information that contain our model and field , and then we wrote regular django views using our serailizer ,created two route which handle two view function one provided a list of all data in api/json format another one provided specific objects of the model

--- Api/urls.py
+++ Api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.apioverview, name='api-overview')
+    path('', views.apioverview, name='api-overview'),
+    path('task-list/', views.taskList, name='task-list'),
+    path('task-detail/<int:pk>/', views.taskDetail, name='task-detail'),
 ]
--- Api/views.py
+++ Api/views.py
@@ -3,6 +3,8 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import TaskSerializer
+from .models import Task
 
 # Create your views here.
 
@@ -10,6 +12,16 @@ from rest_framework.response import Response
 # def apioverview(request):
 #     """result ll be in json format"""
 #     return JsonResponse("API BASE POINT", safe=False)
+
+"""
+Response object, which is a type of TemplateResponse
+@api_view decorator for working with function based views
+These wrappers provide a few bits of functionality
+such as making sure you receive Request instances in your view,
+and adding context to Response objects 
+"""
+
+
 @api_view(['GET'])
 def apioverview(request):
     api_urls = {
@@ -20,3 +32,17 @@ def apioverview(request):
         'Delete': '/task-delete/<str:pk>/',
     }
     return Response(api_urls)
+
+# Django’s serialization framework provides a mechanism for “translating” Django models into other formats.
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+# Getting Specific Object in a Task Model
+@api_view(['GET'])
+def taskDetail(request,pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many=False)
+    return Response(serializer.data)
